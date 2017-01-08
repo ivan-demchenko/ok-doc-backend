bc1abf70834f676fffa5e77e160feffd7c3a00e4
Ivan Demchenko <raqy.style@gmail.com>
2017-01-08 11:55:11 +0100

    Added a first test

---
+++ test/config-generator.test.js
@@ -0,0 +1,17 @@
+const spawn = require('child_process').spawn;
+const joinPath = require('path').join;
+const expect = require('chai').expect;
+
+describe('CLI', () => {
+  it('should output the first question', (done) => {
+    const scriptPath = joinPath(__dirname, '../bin/ok-doc-backend');
+    const be = spawn(scriptPath, []);
+    be.stdout.on('data', (data) => {
+      expect(String(data)).to.equal('? Please, specify where to store the JSON config file: () ')
+      done();
+    });
+    be.stderr.on('data', (data) => {
+      done(data);
+    });
+  });
+});
